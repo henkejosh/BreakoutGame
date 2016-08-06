d1b66f5386eea43ac0774e33b2ba054045db3130
Josh Henke <henke.josh@gmail.com>
2016-08-06 14:36:53 -0700

    Added basic Project files. Began work on Game and Ball classes

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./lib/game.js",
+  output: {
+    path: path.join(__dirname, 'lib'),
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: "node-loader"
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
